b3dfbd437577c99da1c50df26a2aa65fda99ac84
unknown <tjcrimando@gmail.com>
2023-07-29 14:11:27 -0700

    A

--- GUI.py
+++ GUI.py
@@ -1,14 +1,24 @@
 import tkinter as tk
-from PIL import ImageTK, Image
+import maths
+import DATA
 
 
 class MyGui:
 
     def __init__(self):
         self.root = tk.Tk()
-        self.gauge = PIL.ImageTK.PhotoImage(Image.open("./Images./gauge.png"))
-        self.my_label = tk.Label(image=self.gauge)
-        self.my_label.pack()
+        self.root.attributes("-fullscreen",True)
+        
+        self.maxspeed = tk.Label(self.root, text="Max:", fg="red")
+        self.maxspeed.grid(row=0, column=0,)
+
+        self.currspeed = tk.Label(self.root, text="Curr:", fg="red")
+        self.currspeed.grid(row=1, column=0, sticky=tk.W+tk.E)
+
+        self.avgspeed = tk.Label(self.root, text="Avg:", fg="red")
+        self.avgspeed.grid(row=2, column=0, sticky=tk.W+tk.E)
+
+        self.root.mainloop()
 
 MyGui()
 
